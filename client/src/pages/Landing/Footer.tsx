c78d6dc49ff40a13eaaed49b5d489e6553ac9bcd
juan-josue <juansemail2001@gmail.com>
2025-06-29 16:51:07 -0400

    feat: responsive design for the footer section

--- client/src/pages/Landing/Footer.tsx
+++ client/src/pages/Landing/Footer.tsx
@@ -2,26 +2,26 @@ import { FlaskConical } from "lucide-react";
 
 function Footer() {
   return (
-    <section className="w-full flex flex-col items-center bg-neutral-100">
-      <div className="flex w-full max-w-5xl items-start justify-between text-neutral-500 py-16">
+    <section className="flex flex-col items-center w-full  bg-neutral-100">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-8 w-full max-w-5xl p-16 text-neutral-500">
         <div className="flex gap-2 items-center text-primary">
           <FlaskConical />
           <p className="text-2xl font-bold">Mock Lab</p>
         </div>
         <div className="flex flex-col gap-4">
-          <p className="font-bold text-primary">Website</p>
+          <p className="text-primary font-bold ">Website</p>
           <a>Demo Video</a>
           <a>Features</a>
           <a>Testimonials</a>
         </div>
         <div className="flex flex-col gap-4">
-          <p className="font-bold text-primary">Resources</p>
+          <p className="text-primary font-bold">Resources</p>
           <a>Contact</a>
           <a>Support</a>
           <a>Status</a>
         </div>
         <div className="flex flex-col gap-4">
-          <p className="font-bold text-primary">Social</p>
+          <p className="text-primary font-bold">Social</p>
           <a>LinkedIn</a>
           <a>GitHub</a>
           <a>YouTube</a>
