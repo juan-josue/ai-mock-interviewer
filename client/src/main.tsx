2eab8abad2efb0d6d93664631cbf7939d80bdb04
Jose Eduardo <josenxs12@gmail.com>
2025-08-04 00:04:39 -0400

    feat: integrate ClerkProvider in main application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+
+// Clerk Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
 );
