c122cbb8a3fed743311f4687254798fd636de2f6
Jose Eduardo <josenxs12@gmail.com>
2025-08-04 12:02:48 -0400

    feat: simplify Dashboard component layout by removing authentication logic

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,19 +1,12 @@
-import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
-import Nav from "../Landing/Nav";
+
+
+import AppLayout from "../../components/common/Layout/AppLayout";
+
 function Dashboard() {
   return (
-    <>
-      <Nav />
-      <div className="border-2">
-        <SignedIn>
-          {/* Dashboard content goes here */}
-          <div className="mt-20 flex justify-center items-center h-full w-full">Dashboard stuff</div>
-        </SignedIn>
-      </div>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-    </>
+    <AppLayout>
+      <div>dashboard stuff</div>
+    </AppLayout>
   );
 }
 
